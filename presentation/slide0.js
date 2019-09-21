7bc829cd1a9190e7d250bb2a0db0a4473f379678
Jagannath Bhat <jagannathbhatjb@gmail.com>
2019-09-21 23:14:33 +0530

    add slide1

---
+++ presentation/slide0.js
@@ -0,0 +1,54 @@
+(function () {
+	let currentAction = 0;
+	let currentSlide = 0;
+
+	let nextAction = () => {
+		if (currentAction < actions.length)
+			actions[currentAction++]();
+	};
+
+	let nextSlide = () => {
+		document.getElementById("slide" + currentSlide++).classList.add("slide--done");
+		setTimeout(() => {
+			document.body.style.transform = "translateY(-" + (currentSlide * 100) + "%)";
+		}, 300);
+	};
+
+	let slide1action0 = () => {
+		document.getElementById("tshirt").classList.add("slideup");
+	}
+
+	let slide1action1 = () => {
+		document.getElementById("rain").classList.add("slideleft");
+	}
+
+	let slide1action2 = () => {
+		document.getElementById("tshirt").setAttribute("src", "img/tshirtwet.svg");
+	}
+
+	let slide3action0 = () => {
+		document.getElementById("rain1").classList.add("slideleft");
+	}
+
+	let slide3action1 = () => {
+		document.getElementById("face").setAttribute("src", "img/sad.svg");
+	}
+
+	let actions = [
+		nextSlide,
+		slide1action0,
+		slide1action1,
+		slide1action2,
+		nextSlide,
+		nextSlide,
+		slide3action0,
+		slide3action1
+	];
+
+	document.addEventListener("keydown", event => {
+		if (event.isComposing || event.keyCode === 229)
+			return;
+		if (event.keyCode === 39 || event.keyCode === 34)
+			nextAction();
+	});
+})();
